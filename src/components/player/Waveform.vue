717f848d7d98fd9551a1588957a346b34353b9e2
Hexzii <hexzii@nichijou.moe>
2025-11-11 06:44:48 +0800

    feat: add waveform actions

--- src/components/player/Waveform.vue
+++ src/components/player/Waveform.vue
@@ -1,11 +1,8 @@
 <template>
-  <div @click="waveformClick" ref="waveformContainer" class="w-full h-full p-0.5">
+  <div @click="waveformClick" ref="waveformContainer" class="w-full h-full p-0.5 cursor-pointer">
     <svg :width="width" :height="height" class="w-full h-full ">
       <rect v-for="(h, index) in waveformBars" :key="index" :x="index * BAR_GAP_TOTAL" :y="(height - h * height) / 2"
-        :width="BAR_WIDTH" :height="h * height" rx="2" ry="2" :class="{
-          'fill-primary': index / waveformBars.length < playProgress,
-          'fill-neutral-500': index / waveformBars.length >= playProgress,
-        }" />
+        :width="BAR_WIDTH" :height="h * height" rx="2" ry="2" class="transition-all" :class="getBarClass(index)" />
       <!-- Yeah rounded stuff just brings more lag -->
       <!-- TODO: Clickable cursor icon  -->
 
@@ -18,7 +15,7 @@
 <script setup lang="ts">
 import { Waveform } from "@/utils/types"
 import { interpolateInto } from "@/utils/utils"
-import { useElementSize } from "@vueuse/core"
+import { useElementSize, useMouseInElement, useThrottleFn } from "@vueuse/core"
 import { ref, computed, onMounted, watch } from "vue"
 
 const props = defineProps<{
@@ -36,8 +33,48 @@ const BAR_GAP_TOTAL = BAR_WIDTH + BAR_GAP * 2
 
 const waveformContainer = ref<HTMLDivElement | null>(null)
 const { width, height } = useElementSize(waveformContainer)
+const { x, isOutside } = useMouseInElement(waveformContainer)
 
 const waveform = ref<Waveform | null>(null)
+
+const hoverProgressValue = ref(-1)
+
+const updateHoverProgress = useThrottleFn(() => {
+  if (isOutside.value || width.value === 0) {
+    hoverProgressValue.value = -1
+  } else {
+    hoverProgressValue.value = x.value / width.value
+  }
+}, 50)
+
+// Watch for mouse position changes and update hover progress
+watch([x, isOutside, width], updateHoverProgress, { immediate: true })
+
+function getBarClass(index: number) {
+  const barPosition = index / waveformBars.value.length
+  const isPlayed = barPosition < props.playProgress
+
+  // If not hovering, use default colors
+  if (isOutside.value || hoverProgressValue.value < 0) {
+    return isPlayed ? "fill-secondary" : "fill-neutral-500"
+  }
+
+  const isHovered = barPosition <= hoverProgressValue.value
+
+  if (isPlayed) {
+    if (isHovered) {
+      return "fill-secondary opacity-100 "
+    } else {
+      return "fill-secondary opacity-50"
+    }
+  } else {
+    if (isHovered) {
+      return "fill-primary opacity-50 "
+    } else {
+      return "fill-neutral-500 opacity-50"
+    }
+  }
+}
 
 const waveformBars = computed(() => {
   let samples = waveform.value?.samples || [
